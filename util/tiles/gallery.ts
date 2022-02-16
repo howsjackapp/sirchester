794fb39bfcc9a1b280f8ca3b37e7f36ec56afe9e
Amaury M <1293565+amaurym@users.noreply.github.com>
2022-02-16 15:33:47 +0100

    fix: Add sirchester's own proxy

--- util/tiles/gallery.ts
+++ util/tiles/gallery.ts
@@ -1,5 +1,6 @@
 import google_ddg from '../../gallery/google_ddg.json';
 import google_reddit_ddg from '../../gallery/google_reddit_ddg.json';
+import google_reddit_marginalia from '../../gallery/google_reddit_marginalia.json';
 import sp_reddit_marginalia from '../../gallery/sp_reddit_marginalia.json';
 import type { TileState } from './tiles';
 
@@ -13,5 +14,6 @@ import type { TileState } from './tiles';
 export const gallery: Record<string, TileState> = {
 	google_ddg: google_ddg as TileState,
 	google_reddit_ddg: google_reddit_ddg as TileState,
+	google_reddit_marginalia: google_reddit_marginalia as TileState,
 	sp_reddit_marginalia: sp_reddit_marginalia as TileState,
 };
